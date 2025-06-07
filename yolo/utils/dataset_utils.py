deb225a631f3d1e346f50ea5ded5c36aa2589e7c
Rudiio <fidacyrillerudio@gmail.com>
2025-06-07 14:22:56 +0200

    fix(data-loading): 🐛 make `bboxx ̀ prioritary before `segmentation`

--- yolo/utils/dataset_utils.py
+++ yolo/utils/dataset_utils.py
@@ -51,13 +51,23 @@ def create_image_metadata(labels_path: str) -> Tuple[Dict[str, List], Dict[str, 
     """
     with open(labels_path, "r") as file:
         labels_data = json.load(file)
-        id_to_idx = discretize_categories(labels_data.get("categories", [])) if "categories" in labels_data else None
-        annotations_index = organize_annotations_by_image(labels_data, id_to_idx)  # check lookup is a good name?
-        image_info_dict = {Path(img["file_name"]).stem: img for img in labels_data["images"]}
+        id_to_idx = (
+            discretize_categories(labels_data.get("categories", []))
+            if "categories" in labels_data
+            else None
+        )
+        annotations_index = organize_annotations_by_image(
+            labels_data, id_to_idx
+        )  # check lookup is a good name?
+        image_info_dict = {
+            Path(img["file_name"]).stem: img for img in labels_data["images"]
+        }
         return annotations_index, image_info_dict
 
 
-def organize_annotations_by_image(data: Dict[str, Any], id_to_idx: Optional[Dict[int, int]]):
+def organize_annotations_by_image(
+    data: Dict[str, Any], id_to_idx: Optional[Dict[int, int]]
+):
     """
     Use image index to lookup every annotations
     Args:
@@ -101,16 +111,18 @@ def scale_segmentation(
     h, w = image_dimensions["height"], image_dimensions["width"]
     for anno in annotations:
         category_id = anno["category_id"]
-        if "segmentation" in anno:
-            seg_list = [item for sublist in anno["segmentation"] for item in sublist]
-        elif "bbox" in anno:
+        if "bbox" in anno:
             x, y, width, height = anno["bbox"]
             seg_list = [x, y, x + width, y, x + width, y + height, x, y + height]
+        elif "segmentation" in anno:
+            seg_list = [item for sublist in anno["segmentation"] for item in sublist]
 
         scaled_seg_data = (
             np.array(seg_list).reshape(-1, 2) / [w, h]
         ).tolist()  # make the list group in x, y pairs and scaled with image width, height
-        scaled_flat_seg_data = [category_id] + list(chain(*scaled_seg_data))  # flatten the scaled_seg_data list
+        scaled_flat_seg_data = [category_id] + list(
+            chain(*scaled_seg_data)
+        )  # flatten the scaled_seg_data list
         seg_array_with_cat.append(scaled_flat_seg_data)
 
     return seg_array_with_cat
